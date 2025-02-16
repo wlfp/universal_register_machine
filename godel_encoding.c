b1971bcfee9902aae9cacb11c0d109f5f57a2294
Wilf <68606149+w-i-lf@users.noreply.github.com>
2025-02-16 19:21:09 +0000

    refactor: extract numerical pair coding logic & fix: program list uses encoded_program not encoded_instruction as second argument.

--- godel_encoding.c
+++ godel_encoding.c
@@ -1,12 +1,21 @@
 #include "godel_encoding.h"
 
+unsigned long double_angle_encoding(const unsigned long x,
+                                    const unsigned long y) {
+  return (1 << x) * (2 * y + 1);
+}
+
+unsigned long single_angle_encoding(const unsigned long x,
+                                    const unsigned long y) {
+  return double_angle_encoding(x, y) - 1;
+}
+
 unsigned long encode_list(const argument_list list) {
-  unsigned long coded_list = 0;
+  unsigned long encoded_list = 0;
   for (unsigned short i = 0; i < list.length; i++) {
-    unsigned int value_to_push = list.contents[i];
-    coded_list = (1 << value_to_push) * (2 * coded_list + 1);
+    encoded_list = double_angle_encoding(list.contents[i], encoded_list);
   }
-  return coded_list;
+  return encoded_list;
 }
 
 /*
@@ -16,27 +25,26 @@ unsigned long encode_list(const argument_list list) {
 */
 unsigned long encode_program(const program_t program) {
   unsigned long encoded_program = 0;
-  unsigned long encoded_instruction = 0;
+  unsigned long encoded_instruction;
   for (unsigned short i; i < program.num_instructions; i++) {
     switch (program.instructions[i].opcode) {
       case INCREMENT_REGISTER:
         encoded_instruction =
-            (1 << (2 * program.instructions[i].register_index)) *
-            (2 * program.instructions[i].label1_index + 1);
+            double_angle_encoding(2 * program.instructions[i].register_index,
+                                  program.instructions[i].label1_index);
         break;
       case DECREMENT_REGISTER_BRANCH:
-        encoded_instruction =
-            (1 << (2 * program.instructions[i].register_index + 1)) *
-            ((1 << program.instructions[i].label1_index) *
-                 (2 * program.instructions[i].label2_index + 1) -
-             1);
+        encoded_instruction = double_angle_encoding(
+            2 * program.instructions[i].register_index + 1,
+            single_angle_encoding(program.instructions[i].label1_index,
+                                  program.instructions[i].label2_index));
         break;
       case HALT:
         encoded_instruction = 0;
         break;
     }
     encoded_program =
-        (1 << encoded_instruction) * (2 * encoded_instruction + 1);
+        double_angle_encoding(encoded_instruction, encoded_program);
   }
   return encoded_program;
 }
