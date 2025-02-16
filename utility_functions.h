c07385fce5fba7ee64456302ee8b47d5ffc13769
Wilf <68606149+w-i-lf@users.noreply.github.com>
2025-02-16 20:34:23 +0000

    refactor: convert the push, pop, and copy subroutines into macros. feat: add exit_dest to pop, used for signalling that we attempted to pop from an empty list, allows jumping to an arbitrary location in this case.

---
+++ utility_functions.h
@@ -0,0 +1,62 @@
+#define LEN_NON_DESTRUCTIVE_COPY 8
+#define LEN_PUSH_TO_LIST 8
+#define LEN_POP_FROM_LIST 11
+#define TEMP_REG REGISTER_Z_SCRATCH
+#define DUMMY_REG 999
+
+#define NON_DESTRUCTIVE_COPY_MACRO(base, src_reg, dest_reg)                    \
+  instructions[(base) + 0] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           TEMP_REG, (base) + 0, (base) + 1};  \
+  instructions[(base) + 1] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           dest_reg, (base) + 1, (base) + 2};  \
+  instructions[(base) + 2] = (instruction){DECREMENT_REGISTER_BRANCH, src_reg, \
+                                           (base) + 3, (base) + 5};            \
+  instructions[(base) + 3] =                                                   \
+      (instruction){INCREMENT_REGISTER, dest_reg, (base) + 4, DUMMY_REG};      \
+  instructions[(base) + 4] =                                                   \
+      (instruction){INCREMENT_REGISTER, TEMP_REG, (base) + 2, DUMMY_REG};      \
+  instructions[(base) + 5] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           TEMP_REG, (base) + 6, (base) + 7};  \
+  instructions[(base) + 6] =                                                   \
+      (instruction){INCREMENT_REGISTER, src_reg, (base) + 5, DUMMY_REG};       \
+  instructions[(base) + 7] = (instruction){HALT, 999, 999, 999};
+
+#define PUSH_TO_LIST_MACRO(base, item_reg, list_reg)                          \
+  instructions[(base) + 0] = (instruction){DECREMENT_REGISTER_BRANCH,         \
+                                           TEMP_REG, (base) + 0, (base) + 1}; \
+  instructions[(base) + 1] =                                                  \
+      (instruction){INCREMENT_REGISTER, TEMP_REG, (base) + 2, DUMMY_REG};     \
+  instructions[(base) + 2] = (instruction){DECREMENT_REGISTER_BRANCH,         \
+                                           list_reg, (base) + 3, (base) + 4}; \
+  instructions[(base) + 3] =                                                  \
+      (instruction){INCREMENT_REGISTER, TEMP_REG, (base) + 1, DUMMY_REG};     \
+  instructions[(base) + 4] = (instruction){DECREMENT_REGISTER_BRANCH,         \
+                                           TEMP_REG, (base) + 5, (base) + 6}; \
+  instructions[(base) + 5] =                                                  \
+      (instruction){INCREMENT_REGISTER, list_reg, (base) + 4, DUMMY_REG};     \
+  instructions[(base) + 6] = (instruction){DECREMENT_REGISTER_BRANCH,         \
+                                           item_reg, (base) + 2, (base) + 7}; \
+  instructions[(base) + 7] = (instruction){HALT, 999, 999, 999};
+
+#define POP_FROM_LIST_MACRO(base, list_reg, dest_reg, exit_dest)               \
+  instructions[(base) + 0] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           dest_reg, (base) + 0, (base) + 1};  \
+  instructions[(base) + 1] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           TEMP_REG, (base) + 1, (base) + 2};  \
+  instructions[(base) + 2] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           list_reg, (base) + 3, exit_dest};   \
+  instructions[(base) + 3] =                                                   \
+      (instruction){INCREMENT_REGISTER, list_reg, (base) + 4, DUMMY_REG};      \
+  instructions[(base) + 4] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           list_reg, (base) + 5, (base) + 6};  \
+  instructions[(base) + 5] =                                                   \
+      (instruction){INCREMENT_REGISTER, TEMP_REG, (base) + 4, DUMMY_REG};      \
+  instructions[(base) + 6] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           TEMP_REG, (base) + 7, (base) + 9};  \
+  instructions[(base) + 7] = (instruction){DECREMENT_REGISTER_BRANCH,          \
+                                           TEMP_REG, (base) + 8, (base) + 10}; \
+  instructions[(base) + 8] =                                                   \
+      (instruction){INCREMENT_REGISTER, list_reg, (base) + 9, DUMMY_REG};      \
+  instructions[(base) + 9] =                                                   \
+      (instruction){INCREMENT_REGISTER, dest_reg, (base) + 4, DUMMY_REG};      \
+  instructions[(base) + 10] = (instruction){HALT, 999, 999, 999};
